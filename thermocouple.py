c68af8157cb113ebacf87ec52e8a49d5cd1cf4c9
jowood4 <jowood4@gmail.com>
2014-09-17 20:43:51 -0400

    Update thermocouple.py

--- thermocouple.py
+++ thermocouple.py
@@ -19,13 +19,21 @@ class led_controller:
                 wiringpi2.digitalWrite(self.latch,0)
 
         def read_temp(self):
+                data = list()
                 wiringpi2.digitalWrite(self.latch,0)
-                for i in range(16):
+                for i in range(4):
                     #wiringpi2.digitalWrite(self.clockPin,0)
-                    wiringpi2.shiftIn(self.dataPin, self.clockPin, 0)
+                    data.append(wiringpi2.shiftIn(self.dataPin, self.clockPin, 0))
                     #print int(self.led_matrix[i,j])
                     #wiringpi2.digitalWrite(self.dataPin,int(string[k]))
                     #wiringpi2.digitalWrite(self.clockPin,1)
 
                 wiringpi2.digitalWrite(self.clockPin,0)
                 wiringpi2.digitalWrite(self.latch,1)
+                
+                self.thermocouple_temp = (data[0]<<4) + (data[1]>>2)
+                self.fault = data[1] & 0x01
+                self.chip_temp = (data[2]<<4) + (data[3]>>2)
+                self.scv_fault = data[3] & 0x04
+                self.scg_fault = data[3] & 0x02
+                self.oc_fault = data[3] & 0x01
