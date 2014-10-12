136fca681c1a33daecd2a4aa626644c46e22c840
Joe <jowood4@gmail.com>
2014-10-12 19:30:08 -0400

    Queues and threads.  Boom!

--- temp_gui.py
+++ temp_gui.py
@@ -2,7 +2,40 @@
 
 import Image, ImageDraw, ImageTk
 import Tkinter, subprocess, threading, Queue
-import temp_control, wiringpi2
+#import temp_control, wiringpi2
+
+class temp_loop(threading.Thread):
+    def __init__(self, queue):
+	threading.Thread.__init__(self)
+	self.queue = queue
+	self.temp_controller = temp_control.temp_control()
+	self.temp_setting = 25
+	self.read_temp = 25
+	self.run_temp_control = 1
+
+    def set_entry(self, entry):
+	self.entry_read = entry
+	self.entry_read.insert(0, self.temp_setting)
+	
+    def update_read_temp(self):
+	self.entry_read.delete(0, Tkinter.END)
+	self.entry_read.insert(0, self.read_temp)
+
+    def run(self):
+	#print self.read_temp
+	while self.run_temp_control:
+		if self.queue.empty() != True:
+			temp = self.queue.get(0)
+			print temp
+			if temp == -100:
+				self.run_temp_control = 0
+			else:
+				self.temp_setting = temp 
+
+		self.read_temp = self.temp_controller.read_thermo_temp()
+		self.temp_controller.regulate_temp(self.temp_setting, self.read_temp)
+		self.update_read_temp()		
+		wiringpi2.delay(100)
 
 class temp_gui:
     def __init__(self):
@@ -12,11 +45,8 @@ class temp_gui:
         self.root.geometry('%dx%d+%d+%d' % (self.width, self.height, 0,0))
         #self.root.overrideredirect(1)  #take off title bar
 	self.splash_wait = 3000
-	self.temp_setting = 25
-	self.read_temp = 25
-	self.run_temp_control = 0
-	self.thread1 = threading.Thread(target=self.regulate)
-	self.temp_controller = temp_control.temp_control()
+	self.queue = Queue.Queue()
+	self.thread = temp_loop(self.queue)
 
         self.frame = {}
         self.frame['splash_screen'] = Tkinter.Frame(self.root,cursor="none")
@@ -57,15 +87,15 @@ class temp_gui:
         self.quit_button.config(cursor="none",text="Quit", font=("Century Schoolbook L",20))
         self.quit_button.place(width = 100, height = 50, relx = 0.6, rely = 0.7)
 
-	self.entry_read = Tkinter.Entry(self.frame['main_screen'],cursor="none")
-        self.entry_read.config(cursor="none",font=("Century Schoolbook L",20))
-        self.entry_read.place(width = 100, height = 50, relx = 0.1, rely = 0.3)
-	self.entry_read.insert(0, self.temp_setting)
+	entry_read = Tkinter.Entry(self.frame['main_screen'],cursor="none")
+        entry_read.config(cursor="none",font=("Century Schoolbook L",20))
+        entry_read.place(width = 100, height = 50, relx = 0.1, rely = 0.3)
+	self.thread.set_entry(entry_read)	
 
 	self.entry_set = Tkinter.Entry(self.frame['main_screen'],cursor="none")
         self.entry_set.config(cursor="none", font=("Century Schoolbook L",20))
         self.entry_set.place(width = 100, height = 50, relx = 0.1, rely = 0.6)
-	self.entry_set.insert(0, self.temp_setting)
+	self.entry_set.insert(0, self.thread.temp_setting)
 
     def show_splash_screen(self):
         #self.frame['splash_screen'].lift()
@@ -75,35 +105,24 @@ class temp_gui:
 
     def show_main_screen(self):
         self.frame['main_screen'].lift()
-	self.run_temp_control = 1
-	self.thread1.start()
-	self.update_read_temp()
-
-    def regulate(self):
-	#print self.read_temp
-	while self.run_temp_control:
-		self.read_temp = self.temp_controller.read_thermo_temp()
-		self.temp_controller.regulate_temp(self.temp_setting, self.read_temp)
-		wiringpi2.delay(100)
+	self.thread.run_temp_control = 1
+	self.thread.start()
 
     def increase(self):
-	self.temp_setting = self.temp_setting + 1
+	self.thread.temp_setting = self.thread.temp_setting + 1
 	self.entry_set.delete(0, Tkinter.END)
-	self.entry_set.insert(0, self.temp_setting)
+	self.entry_set.insert(0, self.thread.temp_setting)
+	self.queue.put(self.thread.temp_setting)
 
     def decrease(self):
-	self.temp_setting = self.temp_setting - 1
+	self.thread.temp_setting = self.thread.temp_setting - 1
 	self.entry_set.delete(0, Tkinter.END)
-	self.entry_set.insert(0, self.temp_setting)
-
-    def update_read_temp(self):
-	self.entry_read.delete(0, Tkinter.END)
-	self.entry_read.insert(0, self.read_temp)
-	self.root.after(1500, self.update_read_temp)
+	self.entry_set.insert(0, self.thread.temp_setting)
+	self.queue.put(self.thread.temp_setting)
 
     def quit(self):
-	self.run_temp_control = 0
-	self.root.destroy()
+	self.queue.put(-100)
+	#self.root.destroy()
 
 top = temp_gui()
 top.root.mainloop()
