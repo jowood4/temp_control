fdd1f6ba5f7410a0144b562be923f2197445e4c8
Joe <jowood4@gmail.com>
2014-10-12 11:54:02 -0400

    Trying GUI on touchscreen

--- temp_gui.py
+++ temp_gui.py
@@ -11,9 +11,17 @@ class temp_gui:
         self.root.geometry('%dx%d+%d+%d' % (self.width, self.height, 0,0))
         #self.root.overrideredirect(1)  #take off title bar
 	self.splash_wait = 3
+	self.temp_setting = 25
 
-	self.backgnd_pic['splash_screen'] = "/home/pi/photo_booth/start_screen.png"
-	self.backgnd_pic['main_screen'] = "/home/pi/photo_booth/preview.png"
+        self.frame = {}
+        self.frame['splash_screen'] = Tkinter.Frame(self.root,cursor="none")
+        self.frame['main_screen'] = Tkinter.Frame(self.root,cursor="none")
+        self.button = {}
+        self.panel = {}
+        self.tk_backgnd_pic = {}
+	#self.backgnd_pic = {}
+	#self.backgnd_pic['splash_screen'] = "/home/pi/photo_booth/start_screen.png"
+	#self.backgnd_pic['main_screen'] = "/home/pi/photo_booth/preview.png"
 	self.configure_panel('splash_screen')
 	self.configure_main()
 	self.show_splash_screen()
@@ -21,30 +29,47 @@ class temp_gui:
     def configure_panel(self, screen):
         self.frame[screen].place(width = self.width, height = self.height, relx = 0, rely = 0)
         self.panel[screen] = Tkinter.Label(self.frame[screen])
-        picture = Image.open(self.backgnd_pic[screen])
-        self.tk_backgnd_pic[screen] = ImageTk.PhotoImage(picture)
-        self.panel[screen].configure(image = self.tk_backgnd_pic[screen])
+        #picture = Image.open(self.backgnd_pic[screen])
+        #self.tk_backgnd_pic[screen] = ImageTk.PhotoImage(picture)
+        #self.panel[screen].configure(image = self.tk_backgnd_pic[screen])
         self.panel[screen].place(width = self.width, height = self.height, relx = 0, rely = 0)
 
     def configure_main(self):
         self.configure_panel('main_screen')
 
-        self.preview_pic_button = Tkinter.Button(self.frame['preview'],cursor="none")
-        self.preview_pic_button.config(command = self.quit_photo_booth)
-        self.preview_pic_button.place(width = 420, height = 280, relx = 0.44, rely = 0.04)
+        self.up_button = Tkinter.Button(self.frame['main_screen'],cursor="none")
+        self.up_button.config(command = self.increase)
+	self.up_button.config(cursor="none",text="Up", font=("Century Schoolbook L",20))
+        self.up_button.place(width = 100, height = 50, relx = 0.6, rely = 0.3)
         
-        self.snap_pic_button = Tkinter.Button(self.frame['preview'])
-        self.snap_pic_button.config(command = self.show_3)
-        self.snap_pic_button.config(cursor="none",text="Take a picture", font=("Century Schoolbook L",20))
-        self.snap_pic_button.place(width = 250, height = 150, relx = 0.55, rely = 0.6)
+        self.down_button = Tkinter.Button(self.frame['main_screen'])
+        self.down_button.config(command = self.decrease)
+        self.down_button.config(cursor="none",text="Down", font=("Century Schoolbook L",20))
+        self.down_button.place(width = 100, height = 50, relx = 0.6, rely = 0.6)
+
+	self.entry = Tkinter.Entry(self.frame['main_screen'],cursor="none")
+        self.entry.config(cursor="none",text="Down", font=("Century Schoolbook L",20))
+        self.entry.place(width = 100, height = 50, relx = 0.1, rely = 0.4)
+	self.entry.insert(0, self.temp_setting)
 
     def show_splash_screen(self):
-        self.frame['splash_screen'].lift()
-	self.root.update()
-	time.sleep(self.splash_wait)
+        #self.frame['splash_screen'].lift()
+	#self.root.update()
+	#time.sleep(self.splash_wait)
+	self.show_main_screen()
 
     def show_main_screen(self):
         self.frame['main_screen'].lift()
+
+    def increase(self):
+	self.temp_setting = self.temp_setting + 1
+	self.entry.delete(0, Tkinter.END)
+	self.entry.insert(0, self.temp_setting)
+
+    def decrease(self):
+	self.temp_setting = self.temp_setting - 1
+	self.entry.delete(0, Tkinter.END)
+	self.entry.insert(0, self.temp_setting)
 
 top = temp_gui()
 top.root.mainloop()
