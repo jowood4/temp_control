c6bf65388acfb68d946dc08fc99246e31a4f4d95
jowood4 <jowood4@gmail.com>
2014-10-12 16:05:41 +0000

    Added temp control

--- temp_control.py
+++ temp_control.py
@@ -4,25 +4,53 @@ import thermocouple
 import wiringpi2
 
 class temp_control:
-	def __init__(self):
-		SPI_data = 1
-		SPI_clock = 2
-		SPI_latch = 0
-		self.heat_relay = 3
+        def __init__(self):
+                SPI_data = 16
+                SPI_clock = 21
+                SPI_latch = 20
+                self.heat_relay = 12
 
-		wiringpi2.wiringPiSetup()
+                wiringpi2.wiringPiSetup()
 
-		self.thermocouple = thermocouple.thermocouple()
-		self.setup(SPI_data, SPI_clock, SPI_latch)
+                self.thermocouple = thermocouple.thermocouple()
+                self.thermocouple.setup(SPI_data, SPI_clock, SPI_latch)
 
-		wiringpi2.pinMode(self.heat_relay,1)
-		wiringpi2.digitalWrite(self.heat_relay,0)
+                self.relay_state = 0
+                wiringpi2.pinMode(self.heat_relay,1)
+                wiringpi2.digitalWrite(self.heat_relay, self.relay_state)
 
-	def relay_on(self):
-		wiringpi2.digitalWrite(self.heat_relay,1)
+        def relay_on(self):
+                wiringpi2.digitalWrite(self.heat_relay,1)
 
-	def relay_off(self):
-		wiringpi2.digitalWrite(self.heat_relay,0)
+        def relay_off(self):
+                wiringpi2.digitalWrite(self.heat_relay,0)
 
-		
-		
+        def read_thermo_temp(self):
+                self.thermocouple.read_temp()
+                return self.thermocouple.thermocouple_temp * 0.25
+
+        def regulate_temp(self, set_temp):
+                delta = 1
+                timeout = 20
+
+                for i in range(timeout):
+                        read_temp = self.read_thermo_temp()
+                        print read_temp
+                        self.relay_off()
+
+                        if(read_temp <= set_temp - delta):
+                                self.relay_on()
+                                print('Relay ON')
+                                wiringpi2.delay(100)
+                                self.relay_off()
+                                wiringpi2.delay(2000)
+                        elif(read_temp >= set_temp - delta):
+                                self.relay_off()
+                                print('Relay Off')
+
+                        wiringpi2.delay(100)
+
+                self.relay_off()
+                
+                
+                
--- thermocouple.py
+++ thermocouple.py
@@ -4,38 +4,42 @@ import wiringpi2
 
 class thermocouple:
         def __init__(self):
-
-	def setup(self, data, clock, latch):
+                self.data = 0
+        def setup(self, data, clock, latch):
                 self.dataPin = data 
                 self.clockPin = clock
                 self.latchPin = latch
                 
-                wiringpi2.wiringPiSetup()
+                wiringpi2.wiringPiSetupGpio()
                 
-                wiringpi2.pinMode(self.latch,1)
-                wiringpi2.pinMode(self.dataPin,1)
+                wiringpi2.pinMode(self.latchPin,1)
+                wiringpi2.pinMode(self.dataPin,0)
                 wiringpi2.pinMode(self.clockPin,1)
 
-                wiringpi2.digitalWrite(self.dataPin,0)
-                wiringpi2.digitalWrite(self.clockPin,0)
-                wiringpi2.digitalWrite(self.latch,0)
-
-        def read_temp(self):
-                data = list()
-                wiringpi2.digitalWrite(self.latchPin,0)
-                for i in range(4):
-                    #wiringpi2.digitalWrite(self.clockPin,0)
-                    data.append(wiringpi2.shiftIn(self.dataPin, self.clockPin, 0))
-                    #print int(self.led_matrix[i,j])
-                    #wiringpi2.digitalWrite(self.dataPin,int(string[k]))
-                    #wiringpi2.digitalWrite(self.clockPin,1)
-
+##                wiringpi2.digitalWrite(self.dataPin,0)
                 wiringpi2.digitalWrite(self.clockPin,0)
                 wiringpi2.digitalWrite(self.latchPin,1)
+
+        def read_temp(self):
                 
-                self.thermocouple_temp = (data[0]<<4) + (data[1]>>2)
+                data = self.spi_shift()
+                self.thermocouple_temp = (data[0]<<6) + (data[1]>>2)
                 self.fault = data[1] & 0x01
-                self.chip_temp = (data[2]<<4) + (data[3]>>2)
+                self.chip_temp = (data[2]<<4) + (data[3]>>4)
                 self.scv_fault = data[3] & 0x04
                 self.scg_fault = data[3] & 0x02
                 self.oc_fault = data[3] & 0x01
+
+        def spi_shift(self):
+                data = list()
+                wiringpi2.digitalWrite(self.latchPin,0)
+                for i in range(4):
+                        temp_data = 0
+                        for j in range(8):
+                                bit = wiringpi2.digitalRead(self.dataPin)
+                                temp_data = temp_data + (pow(2,7-j) * bit)
+                                wiringpi2.digitalWrite(self.clockPin,1)
+                                wiringpi2.digitalWrite(self.clockPin,0)
+                        data.append(temp_data)
+                wiringpi2.digitalWrite(self.latchPin,1)
+                return data
