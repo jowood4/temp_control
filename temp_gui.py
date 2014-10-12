e13279d0bbe228a467b0f27f809db4fafcdbb13f
Joe <jowood4@gmail.com>
2014-10-12 13:56:14 -0400

    Added read temp window

--- temp_gui.py
+++ temp_gui.py
@@ -81,14 +81,14 @@ class temp_gui:
 	self.temp_setting = self.temp_setting + 1
 	self.entry_set.delete(0, Tkinter.END)
 	self.entry_set.insert(0, self.temp_setting)
-	self.regulate()
+	#self.regulate()
 
     def decrease(self):
 	self.root.after_cancel(self.wait)
 	self.temp_setting = self.temp_setting - 1
 	self.entry_set.delete(0, Tkinter.END)
 	self.entry_set.insert(0, self.temp_setting)
-	self.regulate()
+	#self.regulate()
 
 top = temp_gui()
 top.root.mainloop()
